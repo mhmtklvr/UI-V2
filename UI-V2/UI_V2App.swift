18cc4ba44c96ebdb458e42934163cf0db30818f9
Emirhan KOLVER <emirhanklvr@gmail.com>
2023-08-11 21:55:16 +0300

    Initial Commit

---
+++ UI-V2/UI_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  UI_V2App.swift
+//  UI-V2
+//
+//  Created by MacBook on 11.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct UI_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
